a7d7bc65b6336dd657f92c83a69142cbac9f68e2
Sakshi Manoorkar <126005443+Sakshi443@users.noreply.github.com>
2025-06-24 14:57:33 +0530

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,12 @@
+// src/server.js
+const app = require('./app');
+const dotenv = require('dotenv');
+
+// Load environment variables
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
